8d848a12de5a233b85a74d5bfe09fe5b3088e273
Leo CorGuz <cleonardo21@gmail.com>
2022-07-18 17:38:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ function start() {
   const express = require('express')
   const app = express()
 
-  app.use(express.static(path.join(__dirname, 'public')));
+  
   
   app
+    .use(express.static(path.join(__dirname, 'public')))
     .get('/cpu', cpuBound)
     .get('/memory', memoryBound)
     .get('/io', ioBound)
